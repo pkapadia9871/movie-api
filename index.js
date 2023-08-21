591f6243dfa26e40aea813c4654c9b414ece6077
pkapadia9871 <30789273+pkapadia9871@users.noreply.github.com>
2023-08-21 11:27:53 -0500

    added hashing

--- index.js
+++ index.js
@@ -21,6 +21,23 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const { check, validationResult } = require('express-validator');
+
+const cors = require('cors');
+
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 let auth = require('./auth')(app);
 
 const passport = require('passport');
@@ -96,8 +113,20 @@ app.use(morgan('combined', {stream: accessLogStream}));
   });
 
   /*Allow new users to register */
-  app.post('/users' , async (req, res) => {
+  app.post('/users' ,   [
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], async (req, res) => {
     /*res.send('registered');*/
+      // check the validation object for errors
+      let errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+      }
+    let hashedPassword = Users.hashPassword(req.body.Password);
       await Users.findOne({ Username: req.body.Username })
         .then((user) => {
           if (user) {
@@ -199,8 +228,12 @@ app.use(morgan('combined', {stream: accessLogStream}));
   });
   
   // listen for requests
-  app.listen(8080, () => {
+  /*app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
+  });*/
+  const port = process.env.PORT || 8080;
+  app.listen(port, '0.0.0.0',() => {
+    console.log('Listening on Port ' + port);
   });
 
 
